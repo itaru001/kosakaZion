442a41a69cc14ba23908fde65add5be34d9ba5dc
gazayas <g-zayas@hotmail.com>
2015-06-30 19:14:12 +0900

    最初のコミット

---
+++ Javascript/Javascript.js
@@ -0,0 +1,17 @@
+// コードをここに書く
+
+// 画像のスライダーを入れたい
+
+
+//これを、全部の nav のリンクに対応させてください
+function Mail () {
+	location.href = "mailto:kosakazionchurch@yahoo.co.jp";
+}
+
+function Home() {
+	location.href = "http://www.geocities.jp/kosakazionchurch/"
+}
+
+function Ministries() {
+	location.href = "Ministries/ministriesTop.html"
+}
